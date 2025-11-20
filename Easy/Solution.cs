4cb615ebd5b07cde9c57fe5cac1a3d63ce4121bc
Anzhelika Deriabina <anzhelika.deriabina@gmail.com>
2025-11-20 11:46:27 +0100

    add Fibonacci dp

--- Easy/Solution.cs
+++ Easy/Solution.cs
@@ -379,4 +379,22 @@ public static class Solution
 
         return perim;
     }
+
+    public static int Fib(n)
+    {
+        if ( n == 0 || n == 1 ){
+            return n;
+        }
+
+        int[] mem = new int[n+1];
+        mem[0] = 0;
+        mem[1] = 1;
+
+        for (int i = 2; i<=n; i++){
+            mem[i]=mem[i-1] + mem[i - 2];
+        }
+
+        return mem;
+    }
+
 }
